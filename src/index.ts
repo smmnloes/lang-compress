259aad0ede18a0e1de60828ffea4937126bf837f
Maximilian Lösch <maximilian.loesch@europace.de>
2023-10-16 18:51:09 +0200

    not working...

--- src/index.ts
+++ src/index.ts
@@ -24,6 +24,9 @@ function addWord(root: TrieNode, word: string) {
 }
 
 function printAllWords(root: TrieNode) {
+    if (Object.values(root.links).length === 1) {
+        console.log('Found')
+    }
     if (root.terminal) {
         console.log(root.value)
     }
@@ -34,15 +37,45 @@ function printAllWords(root: TrieNode) {
     }
 }
 
-function initTrie() {
+
+
+function initTrie(): TrieNode {
     const root: TrieNode = {links: {}, terminal: false}
 
     const words: string[] = fs.readFileSync(path.resolve(__dirname, '..', 'resources', 'words_english.txt')).toString().split('\n')
     for (const word of words) {
         addWord(root, word)
     }
-    printAllWords(root)
     console.log('done')
+    return root
 }
 
-initTrie()
+
+function compress(input: string, trie: TrieNode): string {
+    const words = input.toLowerCase().split(' ')
+    const wordsCompressed = words.map(word => {
+        let compressedWord = ''
+        let currentNode = trie
+        for (let i = 0; i < word.length; i++) {
+            const currentChar = word.charAt(i)
+            const nextNode = currentNode.links[currentChar];
+            if (!nextNode) {
+                console.log(`Word ${word} is not known to trie`)
+            } else {
+                if (Object.values(trie.links).length === 1) {
+                    // omit character because it is only one that can follow
+                    currentNode = nextNode
+                } else {
+                    compressedWord += currentChar
+                    currentNode = nextNode
+                }
+            }
+        }
+        return compressedWord
+    })
+    return wordsCompressed.join(' ')
+}
+
+const root = initTrie()
+
+console.log(compress('The maximum size for files that AppCode is able to open, is controlled by the '.toLowerCase(), root))
